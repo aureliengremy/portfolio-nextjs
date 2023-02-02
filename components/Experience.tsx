cfb30857fef809c1575fd8a0c2a83bcb0673c97a
Aurelien Gremy <gremy.aurelien@gmail.com>
2023-02-02 16:00:50 -0500

    feat(About) add type for props

---
+++ components/Experience.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { motion } from "framer-motion";
+
+type Props = {}
+
+const Experience = ({}: Props) => {
+  return (
+    <motion.div
+      initial={{opacity: 0 }}
+      transition={{ duration: 1.5 }}
+      whileInView={{ opacity: 1 }}
+      className="h-screen flex flex-col text-center px-10 justify-evenly mx-auto items-center"
+    >
+        <h3 className="uppercase tracking-[20px] text-gray-500 text-2xl">
+        Experience
+      </h3>
+        
+    </motion.div>
+  )
+}
+
+export default Experience
