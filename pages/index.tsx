47709d41abaa7e828089dc4d3d505fcc8163b8b6
Aurelien Gremy <gremy.aurelien@gmail.com>
2023-02-02 15:55:17 -0500

    feat(About > index.tsx) add component About to index.tsx - add img in public folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import About from '@/components/About';
 import Header from '@/components/Header'
 import Head from 'next/head'
 import Hero from '../components/Hero';
@@ -19,10 +20,13 @@ export default function Home() {
         {/* Header */}
         <Header/>
         {/* Hero */}
-        <section id="hero" className="snap-center">
+        <section id="hero" className="snap-start">
           <Hero />
         </section>
         {/* About */}
+        <section id="about" className="snap-center">
+          <About />
+        </section>
         {/* Experience */}
         {/* Skills */}
         {/* Projects */}
