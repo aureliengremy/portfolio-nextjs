56ab0dac626b47c3671a3f9e849e2c8f721f6acf
Aurelien Gremy <gremy.aurelien@gmail.com>
2023-02-02 19:20:20 -0500

    feat(Experience) create composent

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -9,7 +9,7 @@ const Experience = ({}: Props) => {
       initial={{opacity: 0 }}
       transition={{ duration: 1.5 }}
       whileInView={{ opacity: 1 }}
-      className="h-screen flex flex-col text-center px-10 justify-evenly mx-auto items-center"
+      className="h-screen flex flex-col relative overflow-hidden text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center"
     >
         <h3 className="uppercase tracking-[20px] text-gray-500 text-2xl">
         Experience
