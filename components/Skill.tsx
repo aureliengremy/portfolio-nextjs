760fcf7e46b8d4b4228e0ea3d67fcae2a8f6b6f8
Aurelien Gremy <gremy.aurelien@gmail.com>
2023-02-06 14:25:48 -0500

    feat(Skills & Skill) create components - in progress

---
+++ components/Skill.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+type Props = {}
+
+const Skill = ({}: Props) => {
+  return (
+    <div>Skill</div>
+  )
+}
+export default Skill;
---
+++ components/Skills.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { motion } from "framer-motion";
+import Skill from './Skill';
+
+
+type Props = {}
+
+const Skills = ({}: Props) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      transition={{ duration: 1.5 }}
+      whileInView={{ opacity: 1 }}
+      className="min-h-screen flex flex-col relative text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 justify-center xl:space-y-0 mx-auto items-center"
+    >
+      <h3 className="uppercase tracking-[10px] text-gray-500 text-2xl mb-5">
+        Skills
+      </h3>
+      <h4 className="uppercase tracking-[3px] text-gray-500 text-sm">
+        Hover over a skill for currency profieciency.
+      </h4>
+      <div className="grid grid-cols-4 gap-5">
+        {/* {Skill} */}
+        <Skill/>
+        <Skill/>
+        <Skill/>
+        <Skill/>
+        <Skill/>
+        <Skill/>
+        <Skill/>
+        <Skill/>
+      </div>
+    </motion.div>
+  )
+}
+export default Skills;
