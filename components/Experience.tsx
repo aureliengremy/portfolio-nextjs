8c872fc43160a591ba47dc6eea09badcb7ea2066
Aurelien Gremy <gremy.aurelien@gmail.com>
2023-02-06 14:08:01 -0500

    feat(Experience & ExperienceCard) create components

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,22 +1,27 @@
-import React from 'react'
+import React from "react";
 import { motion } from "framer-motion";
+import ExperienceCard from "../components/ExperienceCard";
 
-type Props = {}
+type Props = {};
 
 const Experience = ({}: Props) => {
   return (
     <motion.div
-      initial={{opacity: 0 }}
+      initial={{ opacity: 0 }}
       transition={{ duration: 1.5 }}
       whileInView={{ opacity: 1 }}
-      className="h-screen flex flex-col relative overflow-hidden text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center"
+      className="h-screen flex flex-col relative overflow-hidden text-left max-w-full px-10 justify-evenly mx-auto items-center"
     >
-        <h3 className="uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="uppercase tracking-[10px] text-gray-500 text-2xl">
         Experience
       </h3>
-        
+      <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory">
+        {/* {Experience} */}
+        <ExperienceCard />
+        <ExperienceCard />
+      </div>
     </motion.div>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
