04f99792c5359d6e0e94d7e8e77f234afeded517
Aurelien Gremy <gremy.aurelien@gmail.com>
2023-02-14 16:56:15 -0500

    fix(change naming) change the file and function name

---
+++ components/WorkExperience.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { motion } from "framer-motion";
+import ExperienceCard from "./ExperienceCard";
+
+type Props = {};
+
+const WorkExperience = ({}: Props) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      transition={{ duration: 1.5 }}
+      whileInView={{ opacity: 1 }}
+      className="h-screen flex flex-col relative overflow-hidden text-left max-w-full px-10 mx-auto items-center"
+    >
+      <div className="mb-6 mt-24">
+        <h3 className="uppercase tracking-[10px] text-gray-500 text-2xl">
+          Experience
+        </h3>
+      </div>
+      <div className="my-auto w-full flex space-x-5 overflow-x-scroll p-4 snap-x snap-mandatory">
+        {/* {Experience} */}
+        <ExperienceCard />
+        <ExperienceCard />
+      </div>
+    </motion.div>
+  );
+};
+
+export default WorkExperience;
