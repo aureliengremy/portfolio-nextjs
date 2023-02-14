03f5a1c4ea10fcfe2a8f8d362d2ae14ad29e440f
Aurelien Gremy <gremy.aurelien@gmail.com>
2023-02-14 17:28:28 -0500

    feat(StaticProps) getStaticProps with Props type who are fetch with typingDef

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,34 @@
-import About from '@/components/About';
-import Experience from '@/components/Experience';
-import Header from '@/components/Header'
-import Skills from '@/components/Skills';
-import Head from 'next/head'
-import Hero from '@/components/Hero';
-import Projects from '@/components/Projects';
-import ContactMe from '@/components/ContactMe';
-import Link from 'next/link';
+import About from "@/components/About";
+import WorkExperience from "@/components/WorkExperience";
+import Header from "@/components/Header";
+import Skills from "@/components/Skills";
+import Head from "next/head";
+import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
+import ContactMe from "@/components/ContactMe";
+import Link from "next/link";
 import { ChevronUpIcon } from "@heroicons/react/24/solid";
+import { GetStaticProps } from "next";
+import { Experience, PageInfo, Project, Skill, Social } from "@/typings";
+import { fetchSkills } from "@/utils/fetchSkills";
+import { fetchExperiences } from "@/utils/fetchExpericence";
+import { fetchPageInfo } from "@/utils/fetchPageInfo";
+import { fetchProjects } from "@/utils/fetchProjects";
+import { fetchSocials } from "@/utils/fetchSocials";
 // import { Inter } from '@next/font/google'
 // import styles from '@/styles/Home.module.css'
 
 // const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+type Props = {
+  pageInfo: PageInfo;
+  experiences: Experience[];
+  projects: Project[];
+  skills: Skill[];
+  socials: Social[];
+};
+
+const Home = ({ pageInfo, experiences, skills, projects, socials }: Props) => {
   return (
     <div className="bg-deep-blue text-white h-screen snap-y snap-mandatory overflow-scroll z-0 overflow-y-scroll overflow-x-hidden scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
       <Head>
@@ -22,9 +37,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className='container mx-auto'>
+      <main className="container mx-auto">
         {/* Header */}
-        <Header/>
+        <Header />
         {/* Hero */}
         <section id="hero" className="snap-start">
           <Hero />
@@ -35,28 +50,53 @@ export default function Home() {
         </section>
         {/* Experience */}
         <section id="experience" className="snap-center">
-          <Experience/>
+          <WorkExperience />
         </section>
         {/* Skills */}
         <section id="skills" className="snap-center">
-          <Skills/>
+          <Skills />
         </section>
         {/* Projects */}
         <section id="projects" className="snap-center">
-          <Projects/>
+          <Projects />
         </section>
         {/* Contact Me */}
         <section id="contact" className="snap-center">
-          <ContactMe/>
+          <ContactMe />
         </section>
         <Link href="#hero">
           <footer className="sticky bottom-5 w-full cursor-pointer">
             <div className="flex items-center justify-center">
-              <ChevronUpIcon className="h-10 w-10 text-slate-500 hover:text-slate-50"/>
+              <ChevronUpIcon className="h-10 w-10 text-slate-500 hover:text-slate-50" />
             </div>
           </footer>
         </Link>
       </main>
     </div>
-  )
-}
+  );
+};
+
+export default Home;
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const pageInfo: PageInfo = await fetchPageInfo();
+  const experiences: Experience[] = await fetchExperiences();
+  const skills: Skill[] = await fetchSkills();
+  const projects: Project[] = await fetchProjects();
+  const socials: Social[] = await fetchSocials();
+
+  return {
+    props: {
+      pageInfo,
+      experiences,
+      skills,
+      projects,
+      socials,
+    },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 10 seconds
+    // if you use: 
+    // revalidate: 10,
+  };
+};
