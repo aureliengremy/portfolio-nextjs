b26daf780782f680dce78e832ec7a4cfba50c4da
Aurelien Gremy <gremy.aurelien@gmail.com>
2023-02-15 15:34:47 -0500

    fix(sanity.ts & next.config.js) resolve image builder and adding nextConfig image params source

--- sanity.ts
+++ sanity.ts
@@ -1,5 +1,5 @@
 import { createClient} from 'next-sanity';
-import createImageUrlBuilder from '@sanity/image-url';
+import imageUrlBuilder from '@sanity/image-url';
 
 export const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
@@ -11,6 +11,8 @@ export const config = {
 // set up the client for fetching data in the getProps page functions
 export const sanityClient = createClient(config);
 
+const builder = imageUrlBuilder(config)
+
 export const urlFor = (source: any) => {
-    createImageUrlBuilder(config).image(source)
+    return builder.image(source)
 }
