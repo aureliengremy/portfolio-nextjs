34d59d5cc4ff44263956c78b1ecc986a4c4f43d2
Aurelien Gremy <gremy.aurelien@gmail.com>
2023-02-23 15:26:09 -0500

    refactor(About) change style and adapt the responsive for small phone

--- components/About.tsx
+++ components/About.tsx
@@ -3,13 +3,13 @@ import { motion } from "framer-motion";
 import { PageInfo } from "@/typings";
 
 type Props = {
-  pageInfo: PageInfo
-}
+  pageInfo: PageInfo;
+};
 
-const About = ({pageInfo}: Props) => {
+const About = ({ pageInfo }: Props) => {
   return (
     <motion.div
-      initial={{opacity: 0 }}
+      initial={{ opacity: 0 }}
       transition={{ duration: 1.5 }}
       whileInView={{ opacity: 1 }}
       className="h-screen flex flex-col text-center px-10 mx-auto items-center"
@@ -35,12 +35,25 @@ const About = ({pageInfo}: Props) => {
           src="/images/Me_smilng-black.png"
           className="mb-5 mx-auto md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-64 xl:w-[500px] xl:h-[600px]"
         />
-        <motion.div className="space-y-10 px-0 md:px-10 xl:m-auto">
-          <h4 className="text-semibold text-4xl">
+        <motion.div
+          initial={{
+            x: 200,
+            opacity: 0,
+          }}
+          transition={{
+            duration: 1.2,
+          }}
+          whileInView={{
+            x: 0,
+            opacity: 1,
+          }}
+          className="space-y-7 md:space-y-10 px-0 md:px-10 xl:m-auto"
+        >
+          <h4 className="text-semibold text-xl md:text-4xl">
             {pageInfo.backgroundInformationTitle}
           </h4>
-          <p className="text-base">
-          {pageInfo.backgroundInformation}
+          <p className="text-sm md:text-base">
+            {pageInfo.backgroundInformation}
           </p>
         </motion.div>
       </div>
