a681c86558a2cd4a6f457c18b498ea2ffca95cb4
Aurelien Gremy <gremy.aurelien@gmail.com>
2023-02-23 16:23:50 -0500

    refactor(ContactMe) change style and adapt the reponsive for small phone

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -17,7 +17,7 @@ const ContactMe = ({pageInfo}: Props) => {
           Contactez moi
         </h3>
       </div>
-      <div className="container mx-auto flex flex-col md:text-left lg:flex-row items-center my-auto">
+      <div className="container mx-5 flex flex-col md:text-left lg:flex-row items-center my-auto">
         <motion.img
           initial={{
             x: -200,
@@ -34,14 +34,14 @@ const ContactMe = ({pageInfo}: Props) => {
           alt="photo de Aurelien Gremy"
           className="hidden md:flex mb-5 lg:m-auto flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-fit md:h-64 lg:w-[500px] lg:h-[300px] xl:w-[600px] xl:h-[400px]"
         />
-        <motion.div className="space-y-10 px-0 lg:px-10 xl:m-auto">
+        <motion.div className="space-y-10 mx-4 lg:px-10 xl:m-auto w-full">
           <div className="flex items-center space-x-5 justify-center">
             <AtSymbolIcon width={122} height={122} />
             {/* <p>gremy.aurelien@gmail.com</p> */}
           </div>
 
           <form
-            className="flex flex-col space-y-2 w-fit mx-auto"
+            className="flex flex-col space-y-2 w-5/6 md:w-fit mx-auto"
             action="https://formsubmit.co/gremy.aurelien@gmail.com"
             // onSubmit={onSubmit}
             method="POST"
@@ -51,7 +51,7 @@ const ContactMe = ({pageInfo}: Props) => {
               name="_token"
               value="gh29HGwAUw3W25ewpNGObPgu2xoNsmcxt2NZaDkx"
             />
-            <div className="flex space-x-2">
+            <div className="flex flex-col space-y-2 md:flex-row md:space-y-0 md:space-x-2">
               <input
                 className="contactInput"
                 placeholder="Votre nom"
