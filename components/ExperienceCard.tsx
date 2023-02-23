d740dcb3bd75b1e2727689aea90442222a65ad95
Aurelien Gremy <gremy.aurelien@gmail.com>
2023-02-23 15:33:32 -0500

    refactor(ExperienceCard) change style and adapt the responsive for small phone

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 const ExperienceCard = ({ experience }: Props) => {
   return (
-    <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#1a1933] p-12 hover:opacity-100 opacity-100 md:opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden">
+    <article className="flex flex-col rounded-lg items-center space-y-4 md:space-y-7 flex-shrink-0 w-[350px] md:w-[600px] xl:w-[900px] snap-center bg-[#1a1933] p-12 hover:opacity-100 opacity-100 md:opacity-40 cursor-pointer transition-opacity duration-200 overflow-scroll md:overflow-hidden">
       <motion.img
         initial={{
           y: -100,
@@ -19,15 +19,15 @@ const ExperienceCard = ({ experience }: Props) => {
         transition={{ duration: 1.2 }}
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true }}
-        className="h-32 xl:h-[150px] object-cover object-center"
+        className="h-16 md:h-32 xl:h-[150px] object-cover object-center"
         src={urlFor(experience?.companyImage).url()}
         alt={experience?.jobTitle + " job"}
       />
 
       <div className="px-0 md:px-10">
-        <h4 className="text-4xl font-light">{experience?.jobTitle}</h4>
-        <p className="font-bold text-2xl mt-1">{experience?.company}</p>
-        <div className="flex space-x-2 my-3">
+        <h4 className="text-xl md:text-4xl font-light">{experience?.jobTitle}</h4>
+        <p className="font-bold text-lg md:text-2xl mt-1">{experience?.company}</p>
+        <div className="flex flex-wrap space-x-2 my-3">
           {/* {Tech used} */}
           {experience?.technologies.map((techno, index) => (
             <Image
@@ -40,7 +40,7 @@ const ExperienceCard = ({ experience }: Props) => {
           ))}
         </div>
         <p className="uppercase py-5 text-gray-300">{new Date(experience.dateStarted).toLocaleDateString('en-us', { year:"numeric", month:"short"})} - {new Date(experience.dateEnded).toLocaleDateString('en-us', { year:"numeric", month:"short"})}</p>
-        <ul className="list-disc space-y-4 ml-5 text-lg">
+        <ul className="list-disc space-y-4 ml-5 text-sm md:text-lg">
           {experience.points.map((point, index)=> (
             <li key={"point"+index}>{point}</li>
 
